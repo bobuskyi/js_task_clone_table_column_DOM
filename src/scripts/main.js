1741d06b23517a9f3100f094ecfd65645016621d
Yurii Bobuskyi <y.bobuskyi@gmail.com>
2025-04-02 23:34:14 +0300

    add clone second column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const tbodyRows = document.querySelectorAll('table tbody tr');
+const theadPosition = document.querySelectorAll('table thead th')[1];
+const theadAge = document.querySelectorAll('table thead th')[3];
+const theadPositionCopy = theadPosition.cloneNode(true);
+const tfootPosition = document.querySelectorAll('table tfoot th')[1];
+const tfootAge = document.querySelectorAll('table tfoot th')[3];
+const tfootPositionCopy = tfootPosition.cloneNode(true);
+
+theadAge.after(theadPositionCopy);
+tfootAge.after(tfootPositionCopy);
+
+for (const row of tbodyRows) {
+  const rowCells = row.querySelectorAll('td');
+  const tdPosition = rowCells[1];
+  const tdAge = rowCells[3];
+  const tdPositionCopy = tdPosition.cloneNode(true);
+
+  tdAge.after(tdPositionCopy);
+}
