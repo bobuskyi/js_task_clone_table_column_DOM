f8a2c9dfec92145efbda1afdbf40266ca04a4717
Yurii Bobuskyi <y.bobuskyi@gmail.com>
2025-04-02 23:41:51 +0300

    add clone second column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,23 @@
 'use strict';
 
 const tbodyRows = document.querySelectorAll('table tbody tr');
+const thead = document.querySelector('table thead tr');
 const theadPosition = document.querySelectorAll('table thead th')[1];
-const theadAge = document.querySelectorAll('table thead th')[3];
+const theadSalary = document.querySelectorAll('table thead th')[4];
 const theadPositionCopy = theadPosition.cloneNode(true);
+const tfoot = document.querySelector('table tfoot tr');
 const tfootPosition = document.querySelectorAll('table tfoot th')[1];
-const tfootAge = document.querySelectorAll('table tfoot th')[3];
+const tfootSalary = document.querySelectorAll('table tfoot th')[4];
 const tfootPositionCopy = tfootPosition.cloneNode(true);
 
-theadAge.after(theadPositionCopy);
-tfootAge.after(tfootPositionCopy);
+thead.insertBefore(theadPositionCopy, theadSalary);
+tfoot.insertBefore(tfootPositionCopy, tfootSalary);
 
 for (const row of tbodyRows) {
   const rowCells = row.querySelectorAll('td');
   const tdPosition = rowCells[1];
-  const tdAge = rowCells[3];
+  const tdSalary = rowCells[4];
   const tdPositionCopy = tdPosition.cloneNode(true);
 
-  tdAge.after(tdPositionCopy);
+  row.insertBefore(tdPositionCopy, tdSalary);
 }
